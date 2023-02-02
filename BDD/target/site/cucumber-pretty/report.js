ab85834990b40c317c522c0a5bac9e5762de2413
riskiferdian <riskiferdian9@gmail.com>
2023-02-02 15:21:36 +0700

    first commit

---
+++ BDD/target/site/cucumber-pretty/report.js
@@ -0,0 +1,388 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("features/Login.feature");
+formatter.feature({
+  "line": 2,
+  "name": "Login",
+  "description": "As a user I want to login website sauce demo",
+  "id": "login",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Login"
+    }
+  ]
+});
+formatter.before({
+  "duration": 16480634541,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Normal login",
+  "description": "",
+  "id": "login;normal-login",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "User open the website sauce demo",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "User input \"standard_user\" as userName and input \"secret_sauce\" as pass",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "User already on landing page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.verifyLogin()"
+});
+formatter.result({
+  "duration": 415829459,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "standard_user",
+      "offset": 12
+    },
+    {
+      "val": "secret_sauce",
+      "offset": 50
+    }
+  ],
+  "location": "LoginSteps.inputCredential(String,String)"
+});
+formatter.result({
+  "duration": 3830460875,
+  "status": "passed"
+});
+formatter.match({
+  "location": "PurchaseSteps.verifyLandingPage()"
+});
+formatter.result({
+  "duration": 99229625,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 133996208,
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "line": 10,
+  "name": "Invalid login",
+  "description": "",
+  "id": "login;invalid-login",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 11,
+  "name": "User open the website sauce demo",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "User input \"\u003cuserName\u003e\" as userName and input \"\u003cpassword\u003e\" as pass",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "User see error \"\u003cerrorText\u003e\" on login page",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 14,
+  "name": "",
+  "description": "",
+  "id": "login;invalid-login;",
+  "rows": [
+    {
+      "cells": [
+        "userName",
+        "password",
+        "errorText"
+      ],
+      "line": 15,
+      "id": "login;invalid-login;;1"
+    },
+    {
+      "cells": [
+        "standard_user",
+        "",
+        "Epic sadface: Password is required"
+      ],
+      "line": 16,
+      "id": "login;invalid-login;;2"
+    },
+    {
+      "cells": [
+        "",
+        "secret_sauce",
+        "Epic sadface: Username is required"
+      ],
+      "line": 17,
+      "id": "login;invalid-login;;3"
+    },
+    {
+      "cells": [
+        "asd",
+        "asd",
+        "Epic sadface: Username and password do not match any user in this service"
+      ],
+      "line": 18,
+      "id": "login;invalid-login;;4"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 10494350667,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 16,
+  "name": "Invalid login",
+  "description": "",
+  "id": "login;invalid-login;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Login"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "User open the website sauce demo",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "User input \"standard_user\" as userName and input \"\" as pass",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "User see error \"Epic sadface: Password is required\" on login page",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.verifyLogin()"
+});
+formatter.result({
+  "duration": 205596583,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "standard_user",
+      "offset": 12
+    },
+    {
+      "val": "",
+      "offset": 50
+    }
+  ],
+  "location": "LoginSteps.inputCredential(String,String)"
+});
+formatter.result({
+  "duration": 1414038083,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Epic sadface: Password is required",
+      "offset": 16
+    }
+  ],
+  "location": "LoginSteps.verifyErrorText(String)"
+});
+formatter.result({
+  "duration": 54114917,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 110268709,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 7178112834,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Invalid login",
+  "description": "",
+  "id": "login;invalid-login;;3",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Login"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "User open the website sauce demo",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "User input \"\" as userName and input \"secret_sauce\" as pass",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "User see error \"Epic sadface: Username is required\" on login page",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.verifyLogin()"
+});
+formatter.result({
+  "duration": 336227459,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "",
+      "offset": 12
+    },
+    {
+      "val": "secret_sauce",
+      "offset": 37
+    }
+  ],
+  "location": "LoginSteps.inputCredential(String,String)"
+});
+formatter.result({
+  "duration": 1241404875,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Epic sadface: Username is required",
+      "offset": 16
+    }
+  ],
+  "location": "LoginSteps.verifyErrorText(String)"
+});
+formatter.result({
+  "duration": 55955250,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 111509333,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 6833678709,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 18,
+  "name": "Invalid login",
+  "description": "",
+  "id": "login;invalid-login;;4",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Login"
+    }
+  ]
+});
+formatter.step({
+  "line": 11,
+  "name": "User open the website sauce demo",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 12,
+  "name": "User input \"asd\" as userName and input \"asd\" as pass",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 13,
+  "name": "User see error \"Epic sadface: Username and password do not match any user in this service\" on login page",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "LoginSteps.verifyLogin()"
+});
+formatter.result({
+  "duration": 242340083,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "asd",
+      "offset": 12
+    },
+    {
+      "val": "asd",
+      "offset": 40
+    }
+  ],
+  "location": "LoginSteps.inputCredential(String,String)"
+});
+formatter.result({
+  "duration": 1282831291,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Epic sadface: Username and password do not match any user in this service",
+      "offset": 16
+    }
+  ],
+  "location": "LoginSteps.verifyErrorText(String)"
+});
+formatter.result({
+  "duration": 38551500,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 106689375,
+  "status": "passed"
+});
+});
